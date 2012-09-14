c6c9561e9b99bd439b762216ddad224a5e616ab6
coned <coned@coned-OptiPlex-7010.(none)>
2012-09-14 08:58:46 -0600

    Made basic logger.

--- debug.lib.js
+++ debug.lib.js
@@ -1,3 +1,21 @@
-// JavaScript Document
+/*
+Independent Study Logger
+*/
+var IsLog = {
+	LOG_ON: false, // if LOG_ON = false log messages are logged, and not otherwise
 
-function Logger
+	/*
+	Initializes the logger
+	*/
+	init: function() {
+
+	},
+
+	/*
+	Logs the message to the browser console.
+	*/
+	log: function (msg) {
+		if(this.LOG_ON)
+			console.log(msg);
+	}
+};
