2593572a12061476ecc920667b50966a149e856f
Ty Lewis <tyglewis@gmail.com>
2012-09-26 14:03:47 -0600

    Added assertion.

--- debug.lib.js
+++ debug.lib.js
@@ -38,4 +38,17 @@ var IsLog = {
 	}
 };
 
+/*
+This assertion code is from: http://aymanh.com/9-javascript-tips-you-may-not-know#assertion
+*/
 
+function AssertException(message) { this.message = message; }
+AssertException.prototype.toString = function () {
+  return 'AssertException: ' + this.message;
+}
+
+function assert(exp, message) {
+  if (!exp) {
+    throw new AssertException(message);
+  }
+}
